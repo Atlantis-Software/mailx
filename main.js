67bc41825dbfe198eae4527bf5a87152c5c62216
Alexandre Tiertant <a.tiertant@atlantis-software.net>
2015-01-15 10:31:06 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+
+var Transport = require("./lib/transport");
+var Store = require("./lib/store");
+var Message = require("./lib/message");
+
+
+module.exports = {
+    transport: function(host, port, login, password) {
+        return new Transport(host, port, login, password);
+    },
+    store: function(protocol, host, port, login, password) {
+        return new Store(protocol, host, port, login, password);
+    },
+    message: Message.Create
+};
+
+
